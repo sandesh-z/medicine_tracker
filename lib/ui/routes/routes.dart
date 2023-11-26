e024465040dfc333103925f5d6790986523b9482
Sandace <gsandace@gmail.com>
2023-11-26 15:06:00 +0545

    auto route pagckag added

--- lib/ui/routes/routes.dart
+++ lib/ui/routes/routes.dart
@@ -1,23 +1,14 @@
-import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
-import 'package:medicine_tracker/ui/pages/home/home_page.dart';
+import 'package:auto_route/auto_route.dart';
 import 'package:medicine_tracker/ui/pages/add_medicine_page.dart';
+import 'package:medicine_tracker/ui/pages/home/home_page.dart';
 
-final GoRouter router = GoRouter(
-  routes: <RouteBase>[
-    GoRoute(
-      path: '/',
-      builder: (BuildContext context, GoRouterState state) {
-        return const HomePage();
-      },
-      routes: <RouteBase>[
-        GoRoute(
-          path: 'add',
-          builder: (BuildContext context, GoRouterState state) {
-            return const AddMedicinePage();
-          },
-        ),
-      ],
-    ),
-  ],
-);
+part 'routes.gr.dart';
+
+@AutoRouterConfig(replaceInRouteName: 'Page,Route')
+class AppRouter extends _$AppRouter {
+  @override
+  List<AutoRoute> get routes => [
+        AutoRoute(page: HomeRoute.page, initial: true),
+        AutoRoute(page: AddMedicineRoute.page),
+      ];
+}
---
+++ lib/ui/routes/routes.gr.dart
@@ -0,0 +1,59 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+// **************************************************************************
+// AutoRouterGenerator
+// **************************************************************************
+
+// ignore_for_file: type=lint
+// coverage:ignore-file
+
+part of 'routes.dart';
+
+abstract class _$AppRouter extends RootStackRouter {
+  // ignore: unused_element
+  _$AppRouter({super.navigatorKey});
+
+  @override
+  final Map<String, PageFactory> pagesMap = {
+    AddMedicineRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const AddMedicinePage(),
+      );
+    },
+    HomeRoute.name: (routeData) {
+      return AutoRoutePage<dynamic>(
+        routeData: routeData,
+        child: const HomePage(),
+      );
+    },
+  };
+}
+
+/// generated route for
+/// [AddMedicinePage]
+class AddMedicineRoute extends PageRouteInfo<void> {
+  const AddMedicineRoute({List<PageRouteInfo>? children})
+      : super(
+          AddMedicineRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'AddMedicineRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
+
+/// generated route for
+/// [HomePage]
+class HomeRoute extends PageRouteInfo<void> {
+  const HomeRoute({List<PageRouteInfo>? children})
+      : super(
+          HomeRoute.name,
+          initialChildren: children,
+        );
+
+  static const String name = 'HomeRoute';
+
+  static const PageInfo<void> page = PageInfo<void>(name);
+}
