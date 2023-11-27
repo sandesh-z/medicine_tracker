d08968e0b67bf4bf0217079d40ae43785dd9ab01
Sandace <gsandace@gmail.com>
2023-11-27 08:31:55 +0545

    local notifcation configuration setup in android and ios

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,12 +1,19 @@
 import UIKit
 import Flutter
-
+import flutter_local_notifications
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
+      if #available(iOS 10.0, *) {
+  UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+}
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  
   ) -> Bool {
+     FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+    GeneratedPluginRegistrant.register(with: registry)
+  }
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
