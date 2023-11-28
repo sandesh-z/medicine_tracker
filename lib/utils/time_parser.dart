41375294d44191c1498f304ed411c248ae320b89
Sandace <gsandace@gmail.com>
2023-11-28 19:42:14 +0545

    time parser accuracy made upto microsecond

--- lib/utils/time_parser.dart
+++ lib/utils/time_parser.dart
@@ -4,7 +4,8 @@ String to24hourTime(String timeAsString) {
   int minute = int.tryParse(timeAsString.split(':').last.split(" ").first) ?? 0;
   int hour = int.tryParse(timeAsString.split(':').toList().first) ?? 0;
 
-  DateTime dateTime = DateTime.now().copyWith(minute: minute, hour: hour);
+  DateTime dateTime = DateTime.now().copyWith(
+      minute: minute, hour: hour, second: 0, millisecond: 0, microsecond: 0);
 
   if (timeAsString.contains("PM") && hour != 12) {
     dateTime = dateTime.add(const Duration(hours: 12));
