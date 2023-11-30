ddc7c5749dc89698aa692fbdfd06fa95dfac44b5
Sandace <gsandace@gmail.com>
2023-11-30 07:37:47 +0545

    feat: Check/Uncheck feature added

--- lib/ui/pages/home/medicine_detail_tile.dart
+++ lib/ui/pages/home/medicine_detail_tile.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:medicine_tracker/features/add_medicine/domain/enitities/medicine_details.dart';
+import 'package:medicine_tracker/ui/widgets/custom_popup_widget.dart';
 import 'package:medicine_tracker/ui/widgets/shadow_box_widget.dart';
+import 'package:medicine_tracker/utils/time_difference_checker.dart';
+import 'package:medicine_tracker/utils/time_parser.dart';
 
 class MedicineDetailTile extends StatelessWidget {
   final MedicineDetails item;
@@ -50,20 +53,62 @@ class MedicineDetailTile extends StatelessWidget {
   }
 }
 
-class CheckboxWithTime extends StatelessWidget {
+class CheckboxWithTime extends StatefulWidget {
   final String time;
   const CheckboxWithTime({super.key, required this.time});
 
   @override
+  State<CheckboxWithTime> createState() => _CheckboxWithTimeState();
+}
+
+class _CheckboxWithTimeState extends State<CheckboxWithTime> {
+  bool isChecked = false;
+  @override
   Widget build(BuildContext context) {
     return Row(mainAxisSize: MainAxisSize.min, children: [
       Checkbox(
-        value: false,
-        onChanged: (checked) {},
+        value: isChecked,
+        checkColor: Colors.green,
+        onChanged: (checked) {
+          final now = DateTime.now();
+          var parsed = to24hourTime(widget.time);
+          var oldDate = DateTime.parse(parsed);
+          if (!canCheck(oldTime: oldDate, currentTime: now)) {
+            showDialog(
+              context: context,
+              builder: (BuildContext context) => const CustomDialog(
+                message:
+                    "You can only check this medicine around 5 minutes before/after medicine taking time",
+                title: "Information",
+                showDismiss: true,
+                dismissText: "OK",
+              ),
+            );
+            return;
+          }
+          if (isChecked) return;
+          showDialog(
+            context: context,
+            builder: (BuildContext context) => CustomDialog(
+              message:
+                  "You can not undo this action. Are you sure you have taken the medicine?",
+              title: "Information",
+              showDismiss: true,
+              actionText: "Yes",
+              onAction: () {
+                setState(() {
+                  isChecked = true;
+                });
+
+                Navigator.pop(context);
+              },
+            ),
+          );
+        },
         fillColor: MaterialStateProperty.all(Colors.white),
       ),
       Text(
-        time,
+        widget.time,
         style: TextStyle(fontSize: 16.sp, color: Colors.white),
       )
     ]);
---
+++ lib/ui/widgets/custom_popup_widget.dart
@@ -0,0 +1,131 @@
+import 'package:flutter/material.dart';
+
+class CustomDialog extends StatelessWidget {
+  final String message;
+  final String title;
+  final IconData icon;
+
+  final Function()? onAction;
+  final String actionText;
+  final String? dismissText;
+  final bool showDismiss;
+
+  const CustomDialog(
+      {super.key,
+      required this.message,
+      required this.title,
+      this.onAction,
+      this.showDismiss = false,
+      this.dismissText,
+      this.actionText = '',
+      this.icon = Icons.info_outline_rounded});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Material(
+        color: Colors.transparent,
+        child: Container(
+          margin: const EdgeInsets.all(40.0),
+          padding: const EdgeInsets.only(top: 40, bottom: 10),
+          decoration: ShapeDecoration(
+              color: Colors.white,
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(15.0))),
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: <Widget>[
+              Icon(icon, size: 80, color: Colors.black26),
+              Flexible(
+                child: Padding(
+                  padding:
+                      const EdgeInsets.only(top: 20.0, left: 20.0, right: 20.0),
+                  child: Text(
+                    title,
+                    style: const TextStyle(
+                      color: Colors.green,
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ),
+              Padding(
+                padding:
+                    const EdgeInsets.only(top: 10.0, left: 30.0, right: 30.0),
+                child: Text(
+                  message,
+                  style: const TextStyle(
+                    color: Colors.black,
+                    fontSize: 12.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              Flexible(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    if (showDismiss)
+                      SizedBox(
+                        height: 40,
+                        width: 120,
+                        child: TextButton(
+                          onPressed: () => Navigator.pop(context),
+                          style: TextButton.styleFrom(
+                            backgroundColor: Colors.red,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ),
+                          ),
+                          child: Text(
+                            dismissText ?? "Cancel",
+                            style: const TextStyle(
+                              fontSize: 10,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    const SizedBox(
+                      width: 8,
+                    ),
+                    if (onAction != null)
+                      SizedBox(
+                        height: 40,
+                        width: 120,
+                        child: TextButton(
+                          onPressed: onAction,
+                          style: TextButton.styleFrom(
+                            backgroundColor: Colors.green,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ),
+                          ),
+                          child: Text(
+                            actionText,
+                            style: const TextStyle(
+                              fontSize: 10,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                  ],
+                ),
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+            ],
+          ),
+        ),
+      ),
+      // ),
+    );
+  }
+}
