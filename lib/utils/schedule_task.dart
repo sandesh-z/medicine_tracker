83129ca0e6be54970fc6cd51722be8b279a04b88
Sandace <gsandace@gmail.com>
2023-12-05 09:56:38 +0545

    schedule task util added to schedule background task periodically

---
+++ lib/utils/schedule_task.dart
@@ -0,0 +1,30 @@
+import 'package:android_alarm_manager_plus/android_alarm_manager_plus.dart';
+
+import 'package:injectable/injectable.dart';
+import 'package:medicine_tracker/core/database/local_database_provider.dart';
+
+@lazySingleton
+class ScheduleTask {
+  static final DBProviderImpl _dbProviderImpl = DBProviderImpl();
+
+  int helloAlarmID = 0;
+  Future<void> initPeriodicTask() async {
+    await AndroidAlarmManager.initialize();
+    await AndroidAlarmManager.periodic(
+      const Duration(hours: 24), //Do the same every 24 hours
+      helloAlarmID++, //Different ID for each alarm
+      resetAllMedicineFlags,
+      wakeup: true,
+      exact: true,
+      allowWhileIdle: true, //the device will be woken up when the alarm fires
+      startAt: DateTime(
+          DateTime.now().year, DateTime.now().month, DateTime.now().day, 1),
+      rescheduleOnReboot: true, //Work after reboot
+    );
+  }
+
+  @pragma('vm:entry-point')
+  static void resetAllMedicineFlags() async {
+    await _dbProviderImpl.resetAllMedicineTakenFlags();
+  }
+}
