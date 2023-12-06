1f0abf3e4761981d042e5c85566705ff1b2823cd
Sandace <gsandace@gmail.com>
2023-12-06 16:42:57 +0545

    function added to check missed schedule

--- lib/utils/time_difference_checker.dart
+++ lib/utils/time_difference_checker.dart
@@ -1,9 +1,33 @@
+import 'package:medicine_tracker/utils/time_parser.dart';
+
 bool canCheck({required DateTime oldTime, required DateTime currentTime}) {
   int difference = ((oldTime.minute - currentTime.minute).abs());
   bool sameHour() => (oldTime.hour == currentTime.hour);
 
   if (difference <= 5 && sameHour()) {
     return true;
+  }
+  return false;
+}
+
+bool isScheduleMissed({required DateTime schedule, required String isChecked}) {
+  DateTime now = DateTime.now();
+  bool isMissed = schedule.isBefore(now);
+  int diff = now.difference(schedule).inMinutes;
+
+  if (isMissed && diff >= 10 && isChecked.contains('false')) {
+    return true;
+  }
+  return false;
+}
+
+bool isAnyScheduleMissed(List<String> schedules, List<String> flags) {
+  for (int j = 0; j < schedules.length; j++) {
+    if (isScheduleMissed(
+        isChecked: flags.isEmpty ? "false" : flags[j],
+        schedule: DateTime.parse(to24hourTime(schedules[j])))) {
+      return true;
+    }
   }
   return false;
 }
